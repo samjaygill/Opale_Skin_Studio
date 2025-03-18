e805eb1996e905940c9249b64f07e7fddc508f69
samjaygill <138401809+samjaygill@users.noreply.github.com>
2025-03-18 21:57:39 +0000

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import './css/Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="sm-links">
+          <div className="footer-section social-media">
 
-export default Footer
+            <div className="social-icons-footer">
+              <a
+                href="https://www.instagram.com/opaleskinstudio/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a
+                href="https://www.tiktok.com/@opaleskinstudio"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-tiktok"></i>
+              </a>
+              <a
+                href="https://www.facebook.com/maisyalicebeauty"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook-square"></i>
+              </a>
+            </div>
+          </div>
+          <a
+            href="https://www.fresha.com/a/opale-skin-studio-foxearth-tuckers-mill-road-jrodaumo/booking?menu=true&pId=2496534&dppub=true&employeeId=4535176"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="book-btn">Book Now</button>
+          </a>
+        </div>
+        <div className="footer-section copyright">
+          <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

1c0b3195ff4fcb3c36c265d20c57e3e3359687a3
samjaygill <138401809+samjaygill@users.noreply.github.com>
2025-04-07 18:10:02 +0100

    added iphone video

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './css/Services.css';
 import ServicesToggle from './ServicesToggle';
-import image2 from './Assets/image2.jpeg';
+// import image2 from './Assets/image2.jpeg';
+import myVideo from './Assets/my-video.mp4';
 import Images from './Images';
 
 const Services = () => {
+  const videoRef = useRef(null); // Creating a reference to the video element
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+      videoRef.current.loop = true;
+    }
+  }, []);
+
   return (
     <e>
       <div className="services">
         <div className="services-container">
           <div className="services-image">
-            <img src={image2} alt="salon" className="salon" />
+            <video
+              ref={videoRef} 
+              className="services-video"
+              muted
+              autoPlay
+              loop
+            >
+              <source src={myVideo} type="video/mp4" />
+            </video>
           </div>
           <div className="services-text">
             <h1 className="services-head">Our Services</h1>
